c9085899680f03181f8e9293c134829800cad2a4
Szxro <szxro@gmail.com>
2024-01-06 13:22:24 -0400

    Add entry point to start the client

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello World");
+import FileService from "./services/fileService";
+import ClientHandler from "./structs/client-handler";
+
+let client = new ClientHandler(new FileService());
+
+client.init();
